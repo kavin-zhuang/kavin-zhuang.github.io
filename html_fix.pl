75a7566d441f370d2ea7038969dad9dba449d084
Jinfeng Zhuang <kavin.zhuang@gmail.com>
2016-11-03 10:55:59 +0800

    修复脚本无法工作的BUG！

--- html_fix.pl
+++ html_fix.pl
@@ -10,13 +10,10 @@ opendir(DIR, "$path") || die "Cannot open dir : $!";
 my @files = readdir(DIR);
 foreach my $file (@files) {
    if(-f "$path/$file") {
-        #print $file."\n";
-        
-        #system("perl -e \"s/MarkdownPad Document/Jinfeng/gi\" ");
-        
+        print $file."\n";
         $pos = index($file, ".html");
         if($pos > 0) {
-             system("perl -p -e \"s/MarkdownPad Document/Jinfeng/gi\" $file");
+             system("perl -pi.bak -e \"s/MarkdownPad Document/Jinfeng/gi\" $file");
         }
     }
 }
